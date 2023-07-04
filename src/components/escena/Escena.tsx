15fdf9f6850ef4ed7723842f0a3a87c7a0117ba8
VladScv <ddcllrnsbrv@uoc.edu>
2023-07-04 12:40:12 +0200

    Exercise 3

--- src/components/escena/Escena.tsx
+++ src/components/escena/Escena.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+import styled from 'styled-components';
+const StyledP = styled.p`
+  margin-bottom: 10px;
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 10px;
+`;
 export const Escena = (props: { text:string[] }) => {
     return (
         <>
             {props.text.map((line)=>(
-                <p>{line}</p>
+                <StyledP>{line}</StyledP>
                 ))
             }
         </>
