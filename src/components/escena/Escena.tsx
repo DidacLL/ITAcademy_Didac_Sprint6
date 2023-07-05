a8b95ae3097d79df7c4abd6f737851e07634cb9e
VladScv <ddcllrnsbrv@uoc.edu>
2023-07-05 09:52:12 +0200

    Exercise 4

--- src/components/escena/Escena.tsx
+++ src/components/escena/Escena.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState} from "react";
 import styled from 'styled-components';
 const StyledP = styled.p`
   margin-bottom: 10px;
@@ -6,11 +6,36 @@ const StyledP = styled.p`
   border-radius: 20px;
   padding: 10px;
 `;
+const StyledSelected = styled.p`
+  margin-bottom: 10px;
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 10px;
+  background-color: salmon;
+`;
+const StyledButtonContainer = styled.div`
+  display: flex;
+`;
+
+const StyledButton = styled.button`
+  flex: 1;
+  min-height: 10vh;
+`;
+const SceneButton = (name:string,action:any) => {
+    return(
+        <StyledButton onClick={action}>{name}</StyledButton>
+    )
+};
 export const Escena = (props: { text:string[] }) => {
+    const [count,setCount]= useState(0)
+    const changeP = (val:number)=>{
+        setCount((prevCount)=> Math.max(Math.min(prevCount + val,props.text.length-1),0));
+    };
     return (
         <>
-            {props.text.map((line)=>(
-                <StyledP>{line}</StyledP>
+            <StyledButtonContainer> {SceneButton("Anterior",()=>changeP(-1))}{SceneButton("Següent",()=>changeP(1))}</StyledButtonContainer>
+            {props.text.map((line,index)=>(
+                index===count?<StyledSelected>{line}</StyledSelected>:<StyledP>{line}</StyledP>
                 ))
             }
         </>
